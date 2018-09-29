f388e0f8565f5469cac9461496e3d7e633009d7c
uniwooni <43693566+uniwooni@users.noreply.github.com>
2018-09-29 16:15:22 +0900

    초급25 - 키보드로 입력받아 콘솔에 출력하기

--- ch01/src/java_study/s_25.java
+++ ch01/src/java_study/s_25.java
@@ -1,10 +1,24 @@
 package java_study;
 
+import java.util.Scanner; // Scanner는 java.util 패키지에 있는 클래스이다(입력, 출력)
+
 public class s_25 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		Scanner scanner = new Scanner(System.in); // 키보드
+		
+		System.out.println("정수를 입력하세요.");
+        int choice = scanner.nextInt();           // 키보드로 정수 입력
+        System.out.println(choice);
+        
+        System.out.println("실수를 입력하세요.");
+        double rchoice = scanner.nextDouble();    // 실수 입력
+        System.out.println(rchoice);
+        
+        System.out.println("문자열을 입력하세요.");
+        String schoice = scanner.next();          // 문자열 입력
+        System.out.println(schoice);
+        
 
 }
